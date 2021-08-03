8877a79df475dc2a19c2f090d31bf62e93f8929f
mayism <lican22@huawei.com>
2021-08-03 20:02:35 +0800

    crash/add

---
+++ xamarin-ios/crashios/ViewController.cs
@@ -0,0 +1,46 @@
+﻿using Foundation;
+using System;
+using UIKit;
+using Huawei.Agconnect.Crash;
+
+namespace crashios0512new
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+        partial void MakeCrash(UIKit.UIButton sender)
+        {
+            AGCCrash.GetSharedInstance().TestIt();
+        }
+
+        partial void CatchException(UIKit.UIButton sender)
+        {
+            AGCCrash.GetSharedInstance().RecordError(new Foundation.NSError());
+        }
+
+        partial void CustomReport(UIKit.UIButton sender)
+        {
+            AGCCrash.GetSharedInstance().SetUserId("testuser");
+            AGCCrash.GetSharedInstance().Log("default info level");
+            AGCCrash.GetSharedInstance().SetCustomValue(new NSString("test"), "this is string value");
+            AGCCrash.GetSharedInstance().LogWithLevel(AGCCrashLogLevel.Warning, "this is warning log level");
+            AGCCrash.GetSharedInstance().SetCustomValue(new NSNumber(123), "this is number");
+
+        }
+    }
+}
---
+++ xamarin-ios/crashios/ViewController.designer.cs
@@ -0,0 +1,28 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace crashios0512new
+{
+	[Register ("ViewController")]
+	partial class ViewController
+	{
+		[Action ("CatchException:")]
+		partial void CatchException (UIKit.UIButton sender);
+
+		[Action ("CustomReport:")]
+		partial void CustomReport (UIKit.UIButton sender);
+
+		[Action ("MakeCrash:")]
+		partial void MakeCrash (UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
